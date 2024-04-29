765d953032560f94a26e6a686ca90635174a8a08
Yorick Peeters <145262422+Yorick-Peeters@users.noreply.github.com>
2024-04-29 17:13:12 +0200

    Alle pagina's visueel klaar

---
+++ src/js/index.js
@@ -0,0 +1,38 @@
+let username= "";
+let loginButton = document.getElementById("login");
+
+const login = (username) => {
+    console.log(username)
+    // build request body
+    const body = { "username": username };
+
+    // URL to send data to
+    const url = "https://project-delta-backend.vercel.app/login";
+    // options for the fetch() methode
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body),
+        redirect: "manual"
+    };
+
+    fetch(url, options)
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.status === "user created") {
+                window.location.replace('https://p-delta.netlify.app/room');
+            } else {
+                let error = document.getElementById("loginError");
+                error.style.opacity = "100%";
+            }
+        })
+        .catch((error) => console.log(error));
+}
+
+
+loginButton.addEventListener("click", () => {
+    username = document.getElementById("username").value;
+    login(username);
+});
